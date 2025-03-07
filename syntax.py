dc2f11cf0d1ab3bb60960cd78825a9dd99ecec0a
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-03-07 14:06:33 -0800

    Initial commit; corrected syntax errors in file syntax.py

--- syntax.py
+++ syntax.py
@@ -1,11 +1,11 @@
 def map_character_frequency(words):
-    char_map = {]
-    for (word in words):
-        for character on word:
+    char_map = {}
+    for word in words:
+        for character in word:
             if character not in char_map:
-                char_map{character} = 1
-          else:
-                char_map(character) += 1
+                char_map[character] = 1
+            else:
+                char_map[character] += 1
     return char_map
 
 def test_map_character_frequency():
@@ -24,3 +24,11 @@ def test_map_character_frequency():
 
 def syntax_errors():
     test_map_character_frequency()
+
+#! Changes made:
+#* Line 2 - replaced ']' with '}'
+#* Line 3 - removed '()' enclosing "word in words"
+#* Line 4 - replaced "on" with "in"
+#* Line 7 - indented else two spaces to the right
+#* Line 6 - replaced '{}' with '[]'
+#* Line 8 - replaced '()' with '[]'
