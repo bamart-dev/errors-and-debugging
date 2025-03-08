d52be3402155435d587e1b939cd271da9ce95a37
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-03-07 22:58:51 -0800

    added if statement on lines 2-3 (get_3rd_value func); added if statement on lines 12-13 (get_last_value func) & corrected misspelling on line 14 (get_last_value func).

--- runtime.py
+++ runtime.py
@@ -1,4 +1,6 @@
 def get_3rd_value(my_list):
+    if len(my_list) < 3:  #* added guard statement; returns None if list length
+        return None       #* is less than 3 (as there would be no 3rd value).
     return my_list[2]
 
 def test_get_3rd_value():
@@ -6,8 +8,10 @@ def test_get_3rd_value():
     assert get_3rd_value([0, 1]) == None
 
 def get_last_value(my_list):
-    item_count = len(my_list)
-    return my_list[itm_count]
+    item_count = len(my_list) - 1
+    if not my_list:    #* added guard statement; returns None if list is empty.
+        return None
+    return my_list[item_count]  #* corrected misspelling of 'item_count'.
 
 def test_get_last_value():
     assert get_last_value([0, 1, 2]) == 2
@@ -18,3 +22,7 @@ def runtime_errors():
     test_get_3rd_value()
     test_get_last_value()
 
+#! Changes made:
+#* Lines 2 & 3 - added if statement to check minimum list length
+#* Lines 12 & 13 - added if statment to check if list is empty
+#* Line 14 - corrected '[item_count]'
